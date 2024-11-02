02297e44b8314509e6990d15c5674de23383ee74
Michal Gawik <gawlik.zator@o2.pl>
2024-11-02 08:03:11 +0100

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <h1>Home</h1>,
+	},
+	{
+		path: '/about',
+		element: <h1>About</h1>,
+	},
+])
+
 function App() {
-	return <div>Welcome</div>
+	return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
