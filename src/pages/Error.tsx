77a3be93f3f92acf82bcb8890a66ab152f17bb5c
Michal Gawik <gawlik.zator@o2.pl>
2024-11-03 06:54:23 +0100

    create

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link, useRouteError } from 'react-router-dom'
 
 const Error = () => {
-	return <div>Error</div>
+	const error = useRouteError()
+
+	return (
+		<div>
+			<h3>Error Page</h3>
+			<Link to={'/'}>Back Home</Link>
+		</div>
+	)
 }
 
 export default Error
