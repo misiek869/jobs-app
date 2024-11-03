f1d54e6a28e7c8f6fbae303e9208f700725aeb6d
Michal Gawik <gawlik.zator@o2.pl>
2024-11-03 06:21:16 +0100

    import pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+	AddJob,
+	Admin,
+	AllJobs,
+	DashboardLayout,
+	DeleteJob,
+	EditJob,
+	Error,
+	HomeLayout,
+	Landing,
+	Login,
+	Profile,
+	Register,
+	Stats,
+} from './pages'
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <h1>Home</h1>,
+		element: <HomeLayout />,
 	},
 	{
-		path: '/about',
-		element: <h1>About</h1>,
+		path: '/register',
+		element: <Register />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/dashboard',
+		element: <DashboardLayout />,
 	},
 ])
 
