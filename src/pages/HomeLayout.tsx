6d14dc86bdb737c8130a351606bc53e45409f0e5
Michal Gawik <gawlik.zator@o2.pl>
2024-11-03 06:34:14 +0100

    add Outlet

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const HomeLayout = () => {
-	return <h1>HomeLayout</h1>
+	return (
+		<>
+			<nav>Nav</nav>
+			<Outlet />
+		</>
+	)
 }
 
 export default HomeLayout
