c635b1223d0d9f769e1ca1f163c2a4c9b2f3fa81
Michal Gawik <gawlik.zator@o2.pl>
2024-11-03 07:51:56 +0100

    add width

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/images/jm_logo.png'
+import mainImg from '../assets/images/main-photo.jpg'
 
 const Landing = () => {
 	return (
-		<div className=''>
+		<section className=''>
 			<nav>
-				
+				<img src={logo} alt='logo' />
 			</nav>
-		</div>
+			<div className=''>
+				<div className=''>
+					<h1>
+						Manage Your <span className=''>Jobs</span>
+					</h1>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+						Exercitationem ab nulla maxime reiciendis, magnam delectus officiis
+						temporibus rem ipsum nemo? Asperiores unde obcaecati animi mollitia
+						esse! Officia beatae a quasi.
+					</p>
+					<Link className='' to={'/register'}>
+						Register
+					</Link>
+					<Link className='' to={'/Login'}>
+						Login / Demo
+					</Link>
+				</div>
+				<img src={mainImg} alt='image with charts' />
+			</div>
+		</section>
 	)
 }
 
