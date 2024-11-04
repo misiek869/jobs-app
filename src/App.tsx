be2169cb5ba9263e32e6eaa658689a5fb790697e
Michal Gawik <gawlik.zator@o2.pl>
2024-11-04 07:55:19 +0100

    create children for DashboardLayout

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,16 @@ const router = createBrowserRouter([
 			{
 				path: 'dashboard',
 				element: <DashboardLayout />,
+				children: [
+					{
+						index: true,
+						element: <AddJob />,
+					},
+					{ path: 'stats', element: <Stats /> },
+					{ path: 'all-jobs', element: <AllJobs /> },
+					{ path: 'profile', element: <Profile /> },
+					{ path: 'admin', element: <Admin /> },
+				],
 			},
 		],
 	},
