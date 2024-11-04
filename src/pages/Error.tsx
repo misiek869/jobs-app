52438272384bb24d7aee85054d15e63b799c0013
Michal Gawik <gawlik.zator@o2.pl>
2024-11-04 06:31:44 +0100

    add logic

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
 import { Link, useRouteError } from 'react-router-dom'
+import errorImg from '../assets/images/error_2.jpg'
 
 const Error = () => {
 	const error = useRouteError()
 
+	if (error.status === 404) {
+		return (
+			<main className='min-h-screen text-center flex flex-col items-center justify-center '>
+				<img src={errorImg} alt='404 error' className='w-[90vw] block mb-8' />
+				<Link className='text-2xl text-gray-900' to={'/dashboard'}>
+					Back Home
+				</Link>
+			</main>
+		)
+	}
+
 	return (
-		<div>
-			<h3>Error Page</h3>
-			<Link to={'/'}>Back Home</Link>
-		</div>
+		<main className='min-h-screen text-center flex flex-col items-center justify-center '>
+			<h3>Something went wrong</h3>
+		</main>
 	)
 }
 
