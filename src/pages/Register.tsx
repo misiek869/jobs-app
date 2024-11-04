9ee6191cf3c541b89fdc022a185d016d413d25e5
Michal Gawik <gawlik.zator@o2.pl>
2024-11-04 06:54:35 +0100

    create form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import Logo from '../components/Logo'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
-	return <div>Register</div>
+	return (
+		<section className='min-h-screen grid items-center'>
+			<form className='max-w-[400px] bg-neutral-100 rounded-sm shadow-md py-8 px-10'>
+				<Logo />
+				<h4 className='text-center mb-6'>Register</h4>
+				<div className=''>
+					<label
+						htmlFor='name'
+						className='block text-lg mb-3 capitalize tracking-wider leading-6'>
+						Name
+					</label>
+					<input
+						className='w-full py-2 px-3 rounded-sm border '
+						type='text'
+						required
+						id='name'
+						name='name'
+						defaultValue={'michal'}
+					/>
+				</div>
+				<button className='' type='submit'>
+					submit
+				</button>
+				<p className='mt-4 text-center leading-6'>
+					Are You a Member?
+					<Link className='text-orange-700 tracking-wider ml-1' to={'/login'}>
+						Login
+					</Link>
+				</p>
+			</form>
+		</section>
+	)
 }
 
 export default Register
