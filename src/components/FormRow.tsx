9abd5200ed069681bf1d3abb3680abe885353823
Michal Gawik <gawlik.zator@o2.pl>
2024-11-04 07:00:01 +0100

    create logic

--- src/components/FormRow.tsx
+++ src/components/FormRow.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+type FormRowProps = {
+	type: string
+	name: string
+	labelText: string
+	defaultValue: string
+}
 
-const FormRow = () => {
-	return <div></div>
+const FormRow = ({ type, name, labelText, defaultValue }: FormRowProps) => {
+	return (
+		<div className=''>
+			<label
+				htmlFor={name}
+				className='block text-lg mb-3 capitalize tracking-wider leading-6'>
+				{labelText || name}
+			</label>
+			<input
+				className='w-full py-2 px-3 rounded-sm border '
+				type={type}
+				required
+				id={name}
+				name={name}
+				defaultValue={defaultValue || ''}
+			/>
+		</div>
+	)
 }
 
 export default FormRow
