606cc7165b0a10d7150504f9a0e7a45cf0e117a4
Michal Gawik <gawlik.zator@o2.pl>
2024-11-06 07:32:25 +0100

    create body and style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,27 @@
+import { AiOutlineMenu } from 'react-icons/ai'
+import Logo from './Logo'
+import { useDashboardContext } from '../pages/DashboardLayout'
+
 const Navbar = () => {
-	return <div>nav</div>
+	const { toggleSidebar } = useDashboardContext()
+
+	return (
+		<nav className='h-24 flex items-center justify-center shadow-md lg:sticky lg:top-0 bg-red-200'>
+			<div className='flex w-[90vw] items-center justify-between lg:w-screen-[90%]'>
+				<button
+					type='button'
+					className='bg-transparent border-transparent text-2xl cursor-pointer flex items-center text-orange-700'
+					onClick={toggleSidebar}>
+					<AiOutlineMenu />
+				</button>
+				<div className=''>
+					<Logo />
+					<h4 className='hidden lg:block'>dashboard</h4>
+				</div>
+				<div className='flex items-center'>toggle/logout</div>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
