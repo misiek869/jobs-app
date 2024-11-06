3192edf793edf4e30507c3e6e3de317b8d884dd8
Michal Gawik <gawlik.zator@o2.pl>
2024-11-06 08:30:21 +0100

    style

--- src/components/SidebarSmall.tsx
+++ src/components/SidebarSmall.tsx
@@ -1,5 +1,53 @@
+import { FaTimes } from 'react-icons/fa'
+import { useDashboardContext } from '../pages/DashboardLayout'
+import Logo from './Logo'
+import links from '../utils/links'
+import { NavLink } from 'react-router-dom'
+
 const SidebarSmall = () => {
-	return <div>small</div>
+	const { showSidebar, toggleSidebar } = useDashboardContext()
+
+	const sidebarContainerStyles =
+		'fixed inset-0 bg-red-100 flex justify-center items-center -z-10 opacity-0 duration-300 visible'
+
+	return (
+		<aside className='lg:hidden'>
+			<div
+				className={
+					showSidebar
+						? `${sidebarContainerStyles} z-50 opacity-100 visible`
+						: `${sidebarContainerStyles}`
+				}>
+				<div className='bg-red-300 w-[90vw] h-[95vh] rounded-sm py-16 px-8 relative flex items-center flex-col'>
+					<button
+						type='button'
+						className='absolute top-3 right-3 bg-transparent text-2xl cursor-pointer text-red-800'
+						onClick={toggleSidebar}>
+						<FaTimes />
+					</button>
+					<header>
+						<Logo />
+					</header>
+					<div className=''>
+						{links.map(link => {
+							const { text, path, icon } = link
+							return (
+								<NavLink
+									to={path}
+									key={text}
+									className=''
+									onClick={toggleSidebar}
+									end>
+									<span className=''>{icon}</span>
+									{text}
+								</NavLink>
+							)
+						})}
+					</div>
+				</div>
+			</div>
+		</aside>
+	)
 }
 
 export default SidebarSmall
