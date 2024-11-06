e0b4c284f1874253ae36cedea055191f44e88bb4
Michal Gawik <gawlik.zator@o2.pl>
2024-11-06 06:45:11 +0100

    create context

--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Navbar, SidebarBig, SidebarSmall } from '../components'
 
+const DashboardContext = createContext()
+
 const DashboardLayout = () => {
+	const user = { name: 'michael' }
+
+	const [showSidebar, setShowSidebar] = useState(false)
+	const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+	const toggleDarkTheme = () => {
+		console.log('toggle dark theme')
+	}
+
+	const toggleSidebar = () => {
+		setShowSidebar(!showSidebar)
+	}
+
+	const logoutUser = async () => {
+		console.log('logout user')
+	}
+
 	return (
-		<section>
-			<main className='grid grid-cols-[1fr] lg:grid-cols-[auto,1fr] '>
-				<SidebarSmall />
-				<SidebarBig />
-				<div>
-					<Navbar />
-					<div className='w-[90vw] my-0 mx-auto py-8 lg:w-[90%]'>
-						<Outlet />
+		<DashboardContext.Provider
+			value={{
+				user,
+				showSidebar,
+				isDarkTheme,
+				toggleDarkTheme,
+				toggleSidebar,
+				logoutUser,
+			}}>
+			<section>
+				<main className='grid grid-cols-[1fr] lg:grid-cols-[auto,1fr] '>
+					<SidebarSmall />
+					<SidebarBig />
+					<div>
+						<Navbar />
+						<div className='w-[90vw] my-0 mx-auto py-8 lg:w-[90%]'>
+							<Outlet />
+						</div>
 					</div>
-				</div>
-			</main>
-		</section>
+				</main>
+			</section>
+		</DashboardContext.Provider>
 	)
 }
+
+export const useDashboardContext = () => useContext(DashboardContext)
 
 export default DashboardLayout
