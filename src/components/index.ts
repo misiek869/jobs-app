3899ddb200422b982ec1208935d6e0ccb5db3a9a
Michal Gawik <gawlik.zator@o2.pl>
2024-11-06 06:17:53 +0100

    add exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Logo } from './Logo'
 export { default as FormRow } from './FormRow'
+export { default as SidebarBig } from './SidebarBig'
+export { default as SidebarSmall } from './SidebarSmall'
+export { default as Navbar } from './Navbar'
