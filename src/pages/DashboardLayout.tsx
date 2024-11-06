abc165e7344f5ee6ffeca192876d96423dfddc53
Michal Gawik <gawlik.zator@o2.pl>
2024-11-06 06:26:47 +0100

    create body and frst styling

--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Navbar, SidebarBig, SidebarSmall } from '../components'
 
 const DashboardLayout = () => {
 	return (
-		<>
-			<Outlet />
-		</>
+		<section>
+			<main className='grid grid-cols-[1fr] lg:grid-cols-[auto,1fr] '>
+				<SidebarSmall />
+				<SidebarBig />
+				<div>
+					<Navbar />
+					<div className='w-[90vw] my-0 mx-auto py-8 lg:w-[90%]'>
+						<Outlet />
+					</div>
+				</div>
+			</main>
+		</section>
 	)
 }
 
