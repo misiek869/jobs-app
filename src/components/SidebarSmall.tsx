a8305a2e5fa5a5f4868fd94822828c6604bc7bf9
Michal Gawik <gawlik.zator@o2.pl>
2024-11-13 06:01:57 +0100

    change color on hover

--- src/components/SidebarSmall.tsx
+++ src/components/SidebarSmall.tsx
@@ -35,7 +35,7 @@ const SidebarSmall = () => {
 								<NavLink
 									to={path}
 									key={text}
-									className='flex items-center py-4 capitalize text-xl tracking-wider font-semibold  text-slate-900 hover:text-slate-800 duration-200'
+									className='flex items-center py-4 capitalize text-xl tracking-wider font-semibold  text-slate-900 hover:text-orange-800 duration-200'
 									onClick={toggleSidebar}
 									end>
 									<span className='text-4xl mr-4 grid place-items-center'>
