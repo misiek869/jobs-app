7604e98560cfbb11dbf05e4ded6b0e6c121904a7
Michal Gawik <gawlik.zator@o2.pl>
2024-11-13 12:34:38 +0100

    create

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,8 @@
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs'
+import { useDashboardContext } from '../pages/DashboardLayout'
+
+const ThemeToggle = () => {
+	return <div></div>
+}
+
+export default ThemeToggle
