edb0f9d1eda8600be50b13ae5d29437292a52b48
Michal Gawik <gawlik.zator@o2.pl>
2024-11-13 10:45:06 +0100

    add dropdown animation

--- src/components/LogoutContainer.tsx
+++ src/components/LogoutContainer.tsx
@@ -2,19 +2,35 @@ import { FaUserCircle, FaCaretDown } from 'react-icons/fa'
 import { useState } from 'react'
 import { useDashboardContext } from '../pages/DashboardLayout'
 
+const btnStyle =
+	'cursor-pointer text-white bg-orange-700 border-none rounded-sm tracking-wider py-3 px-4 shadow-sm duration-300 capitalize inline-block hover:bg-orange-800 hover:shadow-lg'
+
 const LogoutContainer = () => {
 	const [showLogout, setShowLogout] = useState<boolean>(false)
 
 	const { user, logoutUser } = useDashboardContext()
 
 	return (
-		<div>
+		<div className='relative'>
 			<button
 				type='button'
-				className=''
+				className={`${btnStyle} flex items-center justify-center gap-x-2`}
 				onClick={() => setShowLogout(!showLogout)}>
-				a
+				<FaUserCircle />
+				{user?.name}
+				<FaCaretDown />
 			</button>
+			<div
+				className={`absolute top-[45px] left-0 w-full text-center  bg-orange-700 ${
+					showLogout ? 'duration-200' : 'hidden'
+				}`}>
+				<button
+					type='button'
+					className='p-2 bg-transparent text-white w-full h-full cursor-pointer capitalize'
+					onClick={logoutUser}>
+					logout
+				</button>
+			</div>
 		</div>
 	)
 }
